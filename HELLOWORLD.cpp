2e41cdcad98764f8c05540b97b70935bfcc78be5
gifaaryy04 <gifaaryy04@gmail.com>
2025-06-27 09:51:58 +0700

    Gifary / Hello World
    Tugas Hello World - Cloud Computing

---
+++ HELLOWORLD.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "HELLO WORLD" << endl;
+    cout << "SAYA MUHAMMAD GIFARY NEZAR" << endl;
+    cout << "SAYA BERKULIAH DI UNIVERSITAS PAMULANG" << endl;
+
+    return 0;
+}
+
